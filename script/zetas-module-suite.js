031d0bc380652d7cf6f0d709b4193c7ffeb39ee1
Nikolai Kucksdorf <nikolai.kucksdorf@live.de>
2024-08-21 21:03:10 +0200

    Init

---
+++ script/zetas-module-suite.js
@@ -0,0 +1,129 @@
+const moduleName = 'zetas-module-suite';
+const settings = [];
+
+const body = $('body.vtt');
+
+let rollMode;
+
+/********************************** Function Declaration: START **********************************/
+function changeMode(val, name){
+    (val == true && !body.hasClass(name)) ? body.addClass(name) : body.removeClass(name);
+}
+
+function i18n(key){
+    return game.i18n.localize(key);
+}
+
+function buildSettings(){
+    const createSetting = (overrides = {}) => ({
+        id: '',
+        scope: 'client',
+        config: true,
+        type: Boolean,
+        default: true,
+        onChange: '',
+        requiresReload: false,
+        ...overrides  // Override any default values with those provided in the overrides object
+    });
+
+    settings.push(createSetting({
+        id: 'zetas-dark-mode',
+        onChange: val => changeMode(val, 'zetas-dark-mode')
+    }));
+
+    settings.push(createSetting({
+        id: 'zetas-smaller-chat',
+        onChange: val => changeMode(val, 'zetas-smaller-chat')
+    }));
+
+    settings.push(createSetting({
+        id: 'zetas-dir-changes',
+        onChange: val => changeMode(val, 'zetas-dir-changes')
+    }));
+
+    settings.push(createSetting({
+        id: 'zetas-rolltype-buttons',
+        onChange: val => changeMode(val, 'zetas-rolltype-buttons'),
+        requiresReload: false
+    }));
+};
+
+function registerSetting(setting){
+    let prefix = moduleName + "." + setting.id;
+
+    let name = i18n(prefix + ".name");
+    let hint = i18n(prefix + ".hint");
+
+    game.settings.register(moduleName, setting.id, {
+        name: name,
+        hint: hint,
+        scope: setting.scope, //client | world
+        config: true,
+        type: setting.type,
+        default: setting.default,
+        onChange: setting.onChange,
+        requiresReload: setting.requiresReload
+    });
+};
+
+function hideSelect() {
+    $('#chat-controls select').hide();
+    $('#rolltype-buttons').remove();
+};
+
+function addButtons() {
+    const button_bank = '<div id="rolltype-buttons" class="rolltype-container">' +
+                            '<button class="rolltype-button publicroll" name="publicroll" data-tooltip="'+i18n(zetas-module-suite.zetas-rolltype-buttons.publicroll)+'"><i class="fa-solid fa-dice-d20 fa-lg"></i></button>' +
+                            '<button class="rolltype-button gmroll" name="gmroll" data-tooltip="'+i18n(zetas-module-suite.zetas-rolltype-buttons.gmroll)+'"><i class="fa-solid fa-user-secret fa-lg"></i></button>' +
+                            '<button class="rolltype-button blindroll" name="blindroll" data-tooltip="'+i18n(zetas-module-suite.zetas-rolltype-buttons.blindroll)+'"><i class="fa-solid fa-eye-low-vision fa-lg"></i></button>' +
+                            '<button class="rolltype-button selfroll" name="selfroll" data-tooltip="'+i18n(zetas-module-suite.zetas-rolltype-buttons.selfroll)+'"><i class="fa-solid fa-ghost fa-lg"></i></button>' +
+                        '</div>';
+    let type = $('#chat-controls select option:selected')[0].value;
+
+    $('#chat-controls').append(button_bank);
+    $('#rolltype-buttons .rolltype-button.'+ type).addClass('active');
+};
+
+function switchRollType(type) {
+    $('#chat-controls select option:selected').removeAttr('selected');
+    $('#chat-controls select option[value="' + type + '"]').attr('selected');
+    $('#rolltype-buttons .rolltype-button').removeClass('active');
+    $('#rolltype-buttons .rolltype-button.'+ type).addClass('active');
+    localStorage.setItem('core.rollMode', type);
+    rollMode = localStorage.getItem('core.rollMode');
+};
+
+function bindEventListeners() {
+    $('#rolltype-buttons .rolltype-button').on('click', function(e){
+        switchRollType(this.name);
+    });
+};
+/*********************************** Function Declaration: END ***********************************/
+
+/************************************* Hook Declaration: STAR ************************************/
+Hooks.once('init', () => {
+    buildSettings();
+
+    for(let i = 0; i < settings.length; i++){
+        registerSetting(settings[i]);
+        changeMode(game.settings.get(moduleName, settings[i].id), settings[i].id);
+    }
+    rollMode = localStorage.getItem('core.rollMode');
+});
+
+Hooks.on("renderSettingsConfig", () => {
+    for(let i = 0; i < settings.length; i++){
+        $('<div>').addClass('form-group group-header').html(i18n("zetas-module-suite." + settings[i] + ".title"))
+                  .insertBefore($('[name="zetas-module-suite.' + settings[i] + '"]').parents('div.form-group:first'));
+    };
+});
+
+Hooks.on('renderSidebarTab', function(){
+    if(body.hasClass('zetas-rolltype-buttons')){
+        hideSelect();
+        addButtons();
+        bindEventListeners();
+        switchRollType(rollMode);
+    }
+});
+/************************************* Hook Declaration: END *************************************/
